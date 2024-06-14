44601616c2c72c41c331d678ed035c3ab31e37df
Arthur Bréant <arthur@thinkr.fr>
2024-06-14 16:21:20 +0200

    chore: create icons

--- R/icon_component.R
+++ R/icon_component.R
@@ -1,5 +1,15 @@
 #' icon
 #'
+#' @name icon
+#' @description
+#' Remix Icon is a set of open-source neutral-style system symbols for designers and developers.
+#'
+#' @details
+#' ** { size } Integer. Set custom `width` and `height`
+#' ** { color } Character. Set `fill` color
+#' ** { className } Character. Add custom class name
+NULL
+
 #' @importFrom shiny.react reactElement asProps
 #'
 #' @noRd
